40ad962856805fbfbe6ad325aca304ddc68b4081
Edwin Lunandy <populationtwo@gmail.com>
2015-04-13 13:17:55 -0400

    Add controller and set gender scope.

---
+++ app/assets/js/main.js
@@ -0,0 +1,9 @@
+angular.module('ngMadLibs', [])
+	.controller('formController', function($scope){
+		$scope.formData = {},
+		$scope.gender = {
+			pronoun: 'she',
+			possessive: 'her'
+		};
+	})
+
